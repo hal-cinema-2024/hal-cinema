10a7574648349119b427fd2a33a4036a85753d7a
taku10101 <taku1011ht@gmail.com>
2024-04-23 21:29:22 +0900

    ui add

--- admin/src/routes/index.lazy.tsx
+++ admin/src/routes/index.lazy.tsx
@@ -1,3 +1,11 @@
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/table";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/")({
@@ -5,5 +13,27 @@ export const Route = createLazyFileRoute("/")({
 });
 
 function Index() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Table aria-label='Example static collection table'>
+        <TableHeader>
+          <TableColumn>NAME</TableColumn>
+          <TableColumn>ROLE</TableColumn>
+          <TableColumn>STATUS</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow key='1'>
+            <TableCell>Tony Reichert</TableCell>
+            <TableCell>CEO</TableCell>
+            <TableCell>Active</TableCell>
+          </TableRow>
+          <TableRow key='2'>
+            <TableCell>Zoey Lang</TableCell>
+            <TableCell>Technical Lead</TableCell>
+            <TableCell>Paused</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
