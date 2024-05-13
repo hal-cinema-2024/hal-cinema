2e3be29c4ff5b77a053efe8cc980b44fd7b22ae6
hikaru1309 <hikarishiro2001@icloud.com>
2024-05-14 00:44:44 +0900

    sdd: slide css

--- website/src/pages/schedules/Slideshow.tsx
+++ website/src/pages/schedules/Slideshow.tsx
@@ -1,16 +1,80 @@
 import { Carousel, CarouselSlide } from "@yamada-ui/carousel";
+import styled from "styled-components";
+
+type Slide = {
+  bg: string;
+  text: string;
+};
+const slide: Slide[] = [
+  {
+    bg: "primary",
+    text: "1",
+  },
+  {
+    bg: "secondary",
+    text: "2",
+  },
+  {
+    bg: "warning",
+    text: "3",
+  },
+  {
+    bg: "danger",
+    text: "4",
+  },
+  {
+    bg: "primary",
+    text: "5",
+  },
+  {
+    bg: "secondary",
+    text: "6",
+  },
+  {
+    bg: "danger",
+    text: "7",
+  },
+  {
+    bg: "primary",
+    text: "8",
+  },
+  {
+    bg: "secondary",
+    text: "9",
+  },
+];
 
 const Slideshow = () => {
   return (
-    <>
-      <Carousel>
-        <CarouselSlide bg="primary">1</CarouselSlide>
-        <CarouselSlide bg="secondary">2</CarouselSlide>
-        <CarouselSlide bg="warning">3</CarouselSlide>
-        <CarouselSlide bg="danger">4</CarouselSlide>
-      </Carousel>
-    </>
+    <Sdiv>
+      <SCarousel slideSize="33.333%" slidesToScroll={3} loop={false}>
+        {slide.map((item, index) => (
+          <CarouselSlide key={index} bg={item.bg}>
+            <Sp>{item.text}</Sp>
+          </CarouselSlide>
+        ))}
+      </SCarousel>
+    </Sdiv>
   );
 };
 
 export default Slideshow;
+
+const Sdiv = styled.div`
+  width: 1000px;
+  padding: 20px;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  margin-top: 20px;
+`;
+
+const SCarousel = styled(Carousel)`
+  width: 100%;
+`;
+
+const Sp = styled.p`
+  //htmlは.
+  font-size: 40px;
+  color: white;
+  text-align: center;
+`;
