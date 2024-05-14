bfce113675bd62c0c9a73bf2a76af7ca2c161b0a
taku10101 <taku1011ht@gmail.com>
2024-05-14 13:31:53 +0900

    add  component

---
+++ website/src/pages/schedules/components/MovieComponent.tsx
@@ -0,0 +1,6 @@
+
+export const MovieComponent = () => {
+  return (
+    <div>MovieComponent</div>
+  )
+}
---
+++ website/src/pages/schedules/components/ProfileIcon.tsx
@@ -0,0 +1,6 @@
+
+export const ProfileIcon = () => {
+  return (
+    <div>ProfileIcon</div>
+  )
+}
