be5b68705d396273c270cdca1ad73f4beb882a06
hikaru1309 <hikarishiro2001@icloud.com>
2024-05-17 10:28:43 +0900

    add: daliy.ts

--- website/src/pages/schedules/Slideshow.tsx
+++ website/src/pages/schedules/Slideshow.tsx
@@ -1,88 +1,8 @@
 import { useState } from "react";
 import { Carousel, CarouselSlide } from "@yamada-ui/carousel";
 import styled from "styled-components";
-
-type Slide = {
-  bg: string;
-  // text: string;
-  month: string;
-  day: string;
-  week: string;
-};
-const slide: Slide[] = [
-  {
-    bg: "primary",
-    month: "5",
-    day: "1",
-    week: "æœ?",
-  },
-  {
-    bg: "secondary",
-    month: "5",
-    day: "2",
-    week: "ç«",
-  },
-  {
-    bg: "warning",
-    month: "5",
-    day: "3",
-    week: "æ°´",
-  },
-  {
-    bg: "danger",
-    month: "5",
-    day: "4",
-    week: "æœ¨",
-  },
-  {
-    bg: "primary",
-    month: "5",
-    day: "5",
-    week: "é‡?",
-  },
-  {
-    bg: "secondary",
-    month: "5",
-    day: "6",
-    week: "åœ?",
-  },
-  {
-    bg: "danger",
-    month: "5",
-    day: "7",
-    week: "æ—¥",
-  },
-  {
-    bg: "primary",
-    month: "5",
-    day: "8",
-    week: "æœ?",
-  },
-  {
-    bg: "secondary",
-    month: "5",
-    day: "9",
-    week: "ç«",
-  },
-  {
-    bg: "secondary",
-    month: "5",
-    day: "10",
-    week: "æ°´",
-  },
-  {
-    bg: "link",
-    month: "5",
-    day: "11",
-    week: "æœ¨",
-  },
-  {
-    bg: "secondary",
-    month: "5",
-    day: "12",
-    week: "é‡?",
-  },
-];
+import type { SlideType } from "./daliy";
+import { slideData } from "./daliy";
 
 const Slideshow = () => {
   const [color, setColor] = useState<number>(0);
@@ -94,7 +14,7 @@ const Slideshow = () => {
   return (
     <Sdiv>
       <SCarousel slideSize="25%" slidesToScroll={4} loop={false}>
-        {slide.map((item, index) => (
+        {slideData.map((item: SlideType, index: number) => (
           <CarouselSlide
             key={index}
             style={{ cursor: "pointer", position: "relative" }}
