6a24e55da3dd0136d72ebcff208d81ad423a553d
umiiii0322 <unno.shota1222@gmail.com>
2024-05-20 17:19:33 +0900

    add: 映画の一覧表示

--- website/src/pages/movies/Movies.tsx
+++ website/src/pages/movies/Movies.tsx
@@ -1,68 +1,103 @@
 import styled from "styled-components";
 
+{
+  /* <Movies img={} movieName="" /> */
+}
+type MovieType = {
+  // img: File;
+  movieName: string;
+  directorName: string;
+  performerName: string;
+};
+
+const MovieList: MovieType[] = [
+  {
+    movieName: "名探偵コナン 漆黒の白",
+    directorName: "山田太郎",
+    performerName: "江戸川コナン",
+  },
+  {
+    movieName: "名探偵コナン 2",
+    directorName: "山田太郎",
+    performerName: "江戸川コナン",
+  },
+  {
+    movieName: "名探偵コナン 純白の黒",
+    directorName: "山田太郎",
+    performerName: "江戸川コナン",
+  },
+  {
+    movieName: "名探偵コナン 4",
+    directorName: "山田太郎",
+    performerName: "江戸川コナン",
+  },
+];
 
 const Movies = () => {
   return (
     <>
-      <h1>上映映画一覧</h1>
-      <MoviesContainer>
-        <MovieName>
-        <MovieImage>ここに画像</MovieImage>
-          <div>
-            <MovieFlexdiv>
-              <MoviesTitle>作品名：</MoviesTitle>
-              <MoviesTitleName>さくひんめい</MoviesTitleName>
-            </MovieFlexdiv>
-            <MovieFlexdiv>
-              <MoviesDirector>監督名：</MoviesDirector>
-              <MoviesDirectorName>かんとく</MoviesDirectorName>
-            </MovieFlexdiv>
-            <MovieFlexdiv>
-              <MoviePerformer>出演者：</MoviePerformer>
-              <MoviePerformerName>しゅつえんしゃ</MoviePerformerName>
-            </MovieFlexdiv>
-            
-          
-          </div>
-        </MovieName>
-        <DetailsButton>
-          <p>詳細へ  </p>
-          <Arrow></Arrow>
-        </DetailsButton>
-      </MoviesContainer>
-      
+      {MovieList.map((item, index) => (
+        <MoviesContainer key={index}>
+          <MovieName>
+            <MovieImage>ここに画像</MovieImage>
+            <MovieTextDiv>
+              <MovieFlexdiv>
+                <MoviesTitle>作品名：</MoviesTitle>
+                <MoviesTitleName>{item.movieName}</MoviesTitleName>
+              </MovieFlexdiv>
+              <MovieFlexdiv>
+                <MoviesDirector>監督名：</MoviesDirector>
+                <MoviesDirectorName>{item.directorName}</MoviesDirectorName>
+              </MovieFlexdiv>
+              <MovieFlexdiv>
+                <MoviePerformer>出演者：</MoviePerformer>
+                <MoviePerformerName>{item.performerName}</MoviePerformerName>
+              </MovieFlexdiv>
+            </MovieTextDiv>
+          </MovieName>
+          <DetailsButton>
+            <p>詳細へ </p>
+            <Arrow></Arrow>
+          </DetailsButton>
+        </MoviesContainer>
+      ))}
     </>
   );
 };
 
 export default Movies;
 
-// style
+//-------------------------style-------------------------
 // 全体
 const MoviesContainer = styled.div`
   padding: 10px;
-  background-color: #D9D9D9;
+  margin: 0 0 15px 0;
+  background-color: #d9d9d9;
   color: #fff;
   display: flex;
   justify-content: space-between;
   width: 1200px;
 `;
+const MovieTextDiv = styled.div`
+  margin: 0 0 0 25px;
+`;
+
 // 作品の画像（仮）
 const MovieImage = styled.div`
-margin: 0 10px 0 0;
+  margin: 0 10px 0 0;
   height: 250px;
   width: 400px;
   background-color: #002200;
   text-align: center;
-  padding: 100px 0 ;
+  padding: 100px 0;
 `;
 const MovieName = styled.div`
   display: flex;
+  margin: 0 0 0 0;
 `;
 // 詳細の横の矢印
 const DetailsButton = styled.div`
   display: flex;
-  
 `;
 const Arrow = styled.div`
   margin: 0 0 0 10px;
