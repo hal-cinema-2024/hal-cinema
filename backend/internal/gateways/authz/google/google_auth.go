f9f89dcfd8b9d90e6b1b7a013ba78daa71d92da1
murasame29 <oogiriminister@gmail.com>
2024-05-20 08:06:02 +0000

    OAuth2のIFをGoogleで実装

---
+++ backend/internal/gateways/authz/google/google_auth.go
@@ -0,0 +1,51 @@
+package google
+
+import (
+	"context"
+
+	"github.com/hal-cinema-2024/backend/internal/gateways/authz"
+	"golang.org/x/oauth2"
+	v2 "google.golang.org/api/oauth2/v2"
+	"google.golang.org/api/option"
+)
+
+type OAuth2 struct {
+	oac oauth2.Config
+}
+
+func NewOAuth(oac oauth2.Config) *OAuth2 {
+	return &OAuth2{
+		oac: oac,
+	}
+}
+
+func (o *OAuth2) FetchToken(ctx context.Context, code string) (*oauth2.Token, error) {
+	token, err := o.oac.Exchange(ctx, code)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func (o *OAuth2) GetUserInfo(ctx context.Context, token *oauth2.Token) (*authz.UserInfo, error) {
+	client := o.oac.Client(ctx, token)
+
+	service, err := v2.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		return nil, err
+	}
+
+	googleUser, err := service.Userinfo.V2.Me.Get().Context(ctx).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	return &authz.UserInfo{
+		UserID: googleUser.Id,
+		Email:  googleUser.Email,
+		Icon:   googleUser.Picture,
+	}, nil
+}
+
+var _ authz.OAuth2 = (*OAuth2)(nil)
