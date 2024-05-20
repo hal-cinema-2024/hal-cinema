c8deaf3412e728c1574f20816883b91148b03c5f
murasame29 <oogiriminister@gmail.com>
2024-05-20 08:12:42 +0000

    handler登録

---
+++ backend/internal/controller/login/google_login.go
@@ -0,0 +1,42 @@
+package login
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/hal-cinema-2024/backend/internal/framework/hcontext"
+	"github.com/hal-cinema-2024/backend/internal/usecase/google"
+	"github.com/labstack/echo/v4"
+)
+
+type LoginRequest struct {
+	AuthorizationCode string `json:"authorizationCode"`
+}
+
+type LoginResponse struct {
+	UserID string `json:"userId"`
+}
+
+func GoogleLogin(googleLogin *google.Login) func(ctx echo.Context) error {
+	return func(ctx echo.Context) error {
+		var reqBody LoginRequest
+		if err := ctx.Bind(&reqBody); err != nil {
+			log.Println("[ERROR] bad request")
+			return echo.ErrBadRequest
+		}
+
+		userInfo, err := googleLogin.Login(ctx.Request().Context(), reqBody.AuthorizationCode)
+		if err != nil {
+			log.Println("[ERROR] ", err)
+			return echo.ErrInternalServerError
+		}
+
+		ctx.Set(hcontext.UserID.String(), userInfo.UserID)
+
+		// Cookie Sessionを作る
+
+		return ctx.JSON(http.StatusOK, &LoginResponse{
+			UserID: userInfo.UserID,
+		})
+	}
+}
--- backend/internal/framework/router/v1/login.go
+++ backend/internal/framework/router/v1/login.go
@@ -1,5 +1,8 @@
 package v1
 
+import "github.com/hal-cinema-2024/backend/internal/controller/login"
+
 func (v1 *v1Router) login() {
-	v1.engine.POST("/login/google", nil)
+	// TODO: diしてInvokeしてnil から変更する
+	v1.engine.POST("/login/google", login.GoogleLogin(nil))
 }
