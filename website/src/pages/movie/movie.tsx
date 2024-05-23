822d722644e8c26926c2a5532baa0ee4c18713b6
syoufiiii <tanabesyoukaic25.k13@icloud.com>
2024-05-23 15:16:25 +0900

    add test

--- website/src/pages/movie/movie.tsx
+++ website/src/pages/movie/movie.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Kbd } from "@yamada-ui/react";
+import { Image } from "@yamada-ui/react";
 import "./movie.css";
 
 const Movie: React.FC = () => {
   return (
     <div>
-      <h1>sdfasdf</h1>
+      <div>
+        <Kbd>shift</Kbd> + <Kbd>H</Kbd>
+      </div>
+      <div id="img">
+        <Image
+          src="https://dragon-ball-official.com/assets/img/intro/intro_1.png"
+          size="xl"
+        />
+      </div>
     </div>
   );
 };
