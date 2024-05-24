6c78b2422cea386021878d12cc57a8a77a04315e
umiiii0322 <unno.shota1222@gmail.com>
2024-05-24 11:21:05 +0900

    Paginationの変更

--- website/src/pages/movies/Pagenation.tsx
+++ website/src/pages/movies/Pagenation.tsx
@@ -1,5 +1,5 @@
 import { Pagination } from "@nextui-org/pagination";
 
 export function Pagenation() {
-  return <Pagination total={10} initialPage={1} />;
+  return <Pagination showControls total={10} initialPage={1} />;
 }
