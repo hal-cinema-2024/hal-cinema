127265481ff1111a70f1d43f2adeef51f17c608e
syoufiiii <tanabesyoukaic25.k13@icloud.com>
2024-05-25 16:42:34 +0900

    layout: image

--- website/src/pages/movie/movie.tsx
+++ website/src/pages/movie/movie.tsx
@@ -1,19 +1,36 @@
 import React from "react";
-import { Kbd } from "@yamada-ui/react";
-import { Image } from "@yamada-ui/react";
+import { Image as YamadaImage, Box } from "@yamada-ui/react";
 import "./movie.css";
 
 const Movie: React.FC = () => {
   return (
-    <div>
-      <div>
-        <Kbd>shift</Kbd> + <Kbd>H</Kbd>
-      </div>
-      <div id="img">
-        <Image
-          src="https://dragon-ball-official.com/assets/img/intro/intro_1.png"
-          size="xl"
-        />
+    <div id="parent">
+      <h1 id="page-title">作品詳細</h1>
+      <div style={{ display: "flex" }}>
+        <div id="img">
+          <YamadaImage
+            src="https://not-found.com/not-found.png"
+            fallback="https://via.placeholder.com/512"
+            fallbackStrategy="onError"
+            size="xl"
+            style={{ maxWidth: "339px", maxHeight: "480px" }}
+          />
+        </div>
+        <Box
+          id="release-date"
+          p="md"
+          rounded="md"
+          bg="danger"
+          color="white"
+          backgroundColor={"#61c1b0"}
+          style={{ marginLeft: "20px", display: "flex", alignItems: "center" }}
+        >
+          2024
+          <span style={{ fontSize: "1.5rem", marginLeft: "0.5rem" }}>
+            5月1日
+          </span>
+          公開
+        </Box>
       </div>
     </div>
   );
