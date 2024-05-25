44ca936ef5097465745fc1aac658c2fc3590f797
syoufiiii <tanabesyoukaic25.k13@icloud.com>
2024-05-25 21:50:31 +0900

    css: release-data

--- website/src/pages/movie/movie.tsx
+++ website/src/pages/movie/movie.tsx
@@ -16,21 +16,36 @@ const Movie: React.FC = () => {
             style={{ maxWidth: "339px", maxHeight: "480px" }}
           />
         </div>
-        <Box
-          id="release-date"
-          p="md"
-          rounded="md"
-          bg="danger"
-          color="white"
-          backgroundColor={"#61c1b0"}
-          style={{ marginLeft: "20px", display: "flex", alignItems: "center" }}
-        >
-          2024
-          <span style={{ fontSize: "1.5rem", marginLeft: "0.5rem" }}>
-            5月1日
-          </span>
-          公開
-        </Box>
+        <div>
+          <h2 id="work-name">
+            作品名 のび太とのび太とのび太とのび太とのび太と
+          </h2>
+          <Box
+            id="release-date"
+            p="md"
+            rounded="md"
+            bg="danger"
+            color="white"
+            backgroundColor={"#61c1b0"}
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            2024
+            <span
+              style={{
+                fontSize: "1.5rem",
+                marginTop: "-6px",
+                marginLeft: "3px",
+                marginRight: "3px",
+              }}
+            >
+              5/1(金)
+            </span>
+            公開
+          </Box>
+        </div>
       </div>
     </div>
   );
