70b0aefbd4304beab2f49d6ffdf9f4e7b19cd258
hikaru1309 <hikarishiro2001@icloud.com>
2024-05-29 00:50:07 +0900

    add: routeTree

--- website/src/routeTree.gen.ts
+++ website/src/routeTree.gen.ts
@@ -54,22 +54,37 @@ const IndexLazyRoute = IndexLazyImport.update({
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
     '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
       preLoaderRoute: typeof IndexLazyImport
       parentRoute: typeof rootRoute
     }
     '/movie': {
+      id: '/movie'
+      path: '/movie'
+      fullPath: '/movie'
       preLoaderRoute: typeof MovieLazyImport
       parentRoute: typeof rootRoute
     }
     '/movies': {
+      id: '/movies'
+      path: '/movies'
+      fullPath: '/movies'
       preLoaderRoute: typeof MoviesLazyImport
       parentRoute: typeof rootRoute
     }
     '/profile': {
+      id: '/profile'
+      path: '/profile'
+      fullPath: '/profile'
       preLoaderRoute: typeof ProfileLazyImport
       parentRoute: typeof rootRoute
     }
     '/schedules': {
+      id: '/schedules'
+      path: '/schedules'
+      fullPath: '/schedules'
       preLoaderRoute: typeof SchedulesLazyImport
       parentRoute: typeof rootRoute
     }
@@ -78,12 +93,12 @@ declare module '@tanstack/react-router' {
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([
+export const routeTree = rootRoute.addChildren({
   IndexLazyRoute,
   MovieLazyRoute,
   MoviesLazyRoute,
   ProfileLazyRoute,
   SchedulesLazyRoute,
-])
+})
 
 /* prettier-ignore-end */
