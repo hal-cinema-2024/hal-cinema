f932f72198363760438881b9bef40a472cf5b377
taku10101 <taku1011ht@gmail.com>
2024-05-30 11:44:12 +0900

    全体layout fix

--- website/src/routes/profile.lazy.tsx
+++ website/src/routes/profile.lazy.tsx
@@ -1,6 +1,7 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { MovieCard } from "../pages/profile/components/MovieCard";
 import { ProfileCard } from "../pages/profile/components/ProfileCard";
+import styled from "styled-components";
 
 export const Route = createLazyFileRoute("/profile")({
   component: Index,
@@ -8,9 +9,13 @@ export const Route = createLazyFileRoute("/profile")({
 
 function Index() {
   return (
-    <>
+    <Container>
       <ProfileCard />
       <MovieCard />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  margin: 0 auto 20px auto;
+`;
