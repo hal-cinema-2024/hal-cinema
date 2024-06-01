dcc3bb322a28fc09f0ba35e8d220e8a72a0a54ad
hikaru1309 <hikarishiro2001@icloud.com>
2024-06-02 01:03:56 +0900

    Co-authored-by: umiiii0322 <umiiii0322@users.noreply.github.com>

--- admin/src/data_table/Table.tsx
+++ admin/src/data_table/Table.tsx
@@ -4,20 +4,21 @@ import { flexRender } from "@tanstack/react-table";
 import { FC } from "react";
 
 import { Button, Input } from "@nextui-org/react";
+// import { Button } from "@nextui-org/react";
 import { useTable } from "./hooks/useReactTable";
 
 export const DataTable: FC = () => {
   const { table } = useTable();
   return (
     <div>
-      <main>
-        <div
-          style={{
-            margin: "5px",
-          }}
-        >
+      <main style={{ width: "1000px" }}>
+        {/* ----------表示件数絞り込み---------- */}
+        <div className="DispNumContainer">
           <select
-            style={{ margin: "5px" }}
+            style={{
+              margin: "5px",
+              cursor: "pointer",
+            }}
             value={table.getState().pagination.pageSize}
             onChange={(e) => {
               table.setPageSize(Number(e.target.value));
@@ -54,54 +55,69 @@ export const DataTable: FC = () => {
             {">"}
           </Button>
         </div>
+        {/* ----------表示件数絞り込み 終わり---------- */}
+
+        {/* ----------検索・日付絞り込み---------- */}
         <div style={{ padding: "10px" }}>
           <Input
             placeholder="検索"
             value={(table.getState().globalFilter as string) ?? ""}
             onChange={(e) => table.setGlobalFilter(e.target.value)}
           />
-          <Input
-            type="date"
-            value={
-              (
-                table.getColumn("createdAt")?.getFilterValue() as {
-                  from?: string;
-                }
-              )?.from ?? ""
-            }
-            onChange={(e) =>
-              table
-                .getColumn("createdAt")
-                ?.setFilterValue((old: { from?: string; to?: string }) => {
-                  return {
-                    ...old,
-                    from: e.target.value,
-                  };
-                })
-            }
-          />
-          〜
-          <Input
-            type="date"
-            value={
-              (
-                table.getColumn("createdAt")?.getFilterValue() as {
-                  to?: string;
-                }
-              )?.to ?? ""
-            }
-            onChange={(e) =>
-              table
-                .getColumn("createdAt")
-                ?.setFilterValue((old: { from?: string; to?: string }) => {
-                  return {
-                    ...old,
-                    to: e.target.value,
-                  };
-                })
-            }
-          />
+          <div className="SearchContainer">
+            <input
+              placeholder="検索"
+              value={(table.getState().globalFilter as string) ?? ""}
+              onChange={(e) => table.setGlobalFilter(e.target.value)}
+            />
+          </div>
+          <div className="NarrowContainer">
+            <input
+              type="date"
+              value={
+                (
+                  table.getColumn("createdAt")?.getFilterValue() as {
+                    from?: string;
+                  }
+                )?.from ?? ""
+              }
+              onChange={(e) =>
+                table
+                  .getColumn("createdAt")
+                  ?.setFilterValue((old: { from?: string; to?: string }) => {
+                    return {
+                      ...old,
+                      from: e.target.value,
+                    };
+                  })
+              }
+            />
+            〜
+            <input
+              type="date"
+              value={
+                (
+                  table.getColumn("createdAt")?.getFilterValue() as {
+                    to?: string;
+                  }
+                )?.to ?? ""
+              }
+              onChange={(e) =>
+                table
+                  .getColumn("createdAt")
+                  ?.setFilterValue((old: { from?: string; to?: string }) => {
+                    return {
+                      ...old,
+                      to: e.target.value,
+                    };
+                  })
+              }
+            />
+          </div>
         </div>
+        {/* ----------検索・日付絞り込み 終わり---------- */}
+
+        {/* ----------テーブル---------- */}
         <table>
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
