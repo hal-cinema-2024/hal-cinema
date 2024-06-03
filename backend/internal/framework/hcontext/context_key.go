d05a868e52f58786fce3d972c5dc74ee94426349
murasame29 <oogiriminister@gmail.com>
2024-06-03 15:56:14 +0000

    userAgent用のctxkeyを追加

--- backend/internal/framework/hcontext/context_key.go
+++ backend/internal/framework/hcontext/context_key.go
@@ -3,7 +3,8 @@ package hcontext
 type ContextKey string
 
 const (
-	UserID ContextKey = "hal_ciname_user_id"
+	UserID    ContextKey = "hal_ciname_user_id"
+	UserAgent ContextKey = "hal_ciname_user_agent"
 )
 
 func (c ContextKey) String() string {
