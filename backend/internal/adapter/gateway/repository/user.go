2c01b8f16e0b6d48adf51c4d8742e552d4b38d31
murasame29 <oogiriminister@gmail.com>
2024-06-03 16:59:57 +0000

    update user を追加

--- backend/internal/adapter/gateway/repository/user.go
+++ backend/internal/adapter/gateway/repository/user.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"context"
+	"time"
 
 	"github.com/hal-cinema-2024/backend/internal/entities/model"
+	"github.com/hal-cinema-2024/backend/internal/framework/herror"
 	"github.com/hal-cinema-2024/backend/internal/usecase/dai"
 	"gorm.io/gorm"
 )
@@ -27,7 +29,7 @@ func (r *UserRepo) CreateUser(ctx context.Context, user *model.User) (*model.Use
 	return user, nil
 }
 
-func (r *UserRepo) GetUserByID(ctx context.Context, userID string) (*model.User, bool, error) {
+func (r *UserRepo) GetUserByID(ctx context.Context, userID string) (*model.User, error) {
 	var (
 		user  model.User
 		count int64
@@ -35,14 +37,57 @@ func (r *UserRepo) GetUserByID(ctx context.Context, userID string) (*model.User,
 
 	result := r.db.Model(&user).Where("user_id = ?", userID).Find(&user).Count(&count)
 	if result.Error != nil {
-		return nil, false, result.Error
+		return nil, result.Error
 	}
 
 	if count == 0 {
-		return nil, false, nil
+		return nil, herror.ErrResourceNotFound
 	}
 
-	return &user, true, nil
+	return &user, nil
+}
+
+func (r *UserRepo) UpdateUser(ctx context.Context, userID string, user *model.User) (*model.User, error) {
+	saveUser := make(map[string]any)
+	var changeCount int64
+
+	if len(user.FirstName) != 0 {
+		saveUser["first_name"] = user.FirstName
+		changeCount++
+	}
+	if len(user.LastName) != 0 {
+		saveUser["last_name"] = user.LastName
+		changeCount++
+	}
+	if len(user.FirstNameReading) != 0 {
+		saveUser["first_name_reading"] = user.FirstNameReading
+		changeCount++
+	}
+	if len(user.LastNameReading) != 0 {
+		saveUser["last_name_reading"] = user.LastNameReading
+		changeCount++
+	}
+	if user.Age != 0 {
+		saveUser["age"] = user.Age
+		changeCount++
+	}
+	if user.Gender != 0 {
+		saveUser["gender"] = user.Gender
+		changeCount++
+	}
+
+	if changeCount == 0 {
+		return user, herror.ErrNoChange
+	}
+
+	saveUser["updated_at"] = time.Now()
+
+	result := r.db.Model(&model.User{}).Where("user_id = ?", user.UserID).Updates(saveUser).Find(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return user, nil
 }
 
 var _ dai.UserRepo = (*UserRepo)(nil)
