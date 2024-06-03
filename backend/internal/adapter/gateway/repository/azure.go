1cfd408bb1c44937c06f68ed138a98b60b56aa85
sikigasa <km3t1114@yahoo.co.jp>
2024-06-04 02:44:30 +0900

    add: upload_image

--- backend/internal/adapter/gateway/repository/azure.go
+++ backend/internal/adapter/gateway/repository/azure.go
@@ -4,17 +4,24 @@ import (
 	"context"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/hal-cinema-2024/backend/cmd/config"
 )
 
 type AzureBlob struct {
 	client *azblob.Client
 }
 
+func NewAzureBlob() *AzureBlob {
+	return &AzureBlob{}
+}
+
 // Upload to data to blob storage
-func (r *AzureBlob) UploadBlob(ctx context.Context, containerName, blobName string, data []byte) (string, error) {
+func (r *AzureBlob) UploadBlob(ctx context.Context, blobName string, data []byte) (string, error) {
+	var imagePath string = config.Config.Azure.BlobServiceURL + config.Config.Azure.BlobServiceImagePath + "/" + blobName
+	var containerName string = config.Config.Azure.BlobServiceImagePath
 	_, err := r.client.UploadBuffer(ctx, containerName, blobName, data, &azblob.UploadBufferOptions{})
 	if err != nil {
 		return "", err
 	}
-	return blobName, nil
+	return imagePath, nil
 }
