0a07ebf09a63106607217b180f2070b7ae156afa
sikigasa <km3t1114@yahoo.co.jp>
2024-06-04 22:40:07 +0900

    add: delete_blob

--- backend/internal/adapter/gateway/repository/azure.go
+++ backend/internal/adapter/gateway/repository/azure.go
@@ -15,13 +15,24 @@ func NewAzureBlob() *AzureBlob {
 	return &AzureBlob{}
 }
 
+var containerName string = config.Config.Azure.BlobServiceImagePath
+
 // Upload to data to blob storage
 func (r *AzureBlob) UploadBlob(ctx context.Context, blobName string, data []byte) (string, error) {
 	var imagePath string = config.Config.Azure.BlobServiceURL + config.Config.Azure.BlobServiceImagePath + "/" + blobName
-	var containerName string = config.Config.Azure.BlobServiceImagePath
 	_, err := r.client.UploadBuffer(ctx, containerName, blobName, data, &azblob.UploadBufferOptions{})
 	if err != nil {
 		return "", err
 	}
 	return imagePath, nil
+}
+
+// delete blob from blob storage
+func (r *AzureBlob) DeleteBlob(ctx context.Context, blobName string) error {
+	var containerName string = config.Config.Azure.BlobServiceImagePath
+	_, err := r.client.DeleteBlob(ctx, containerName, blobName, &azblob.DeleteBlobOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
 }
