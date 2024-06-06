9d65fe9c7ad7096a08f4f8bebe04c5597bcdbb7c
sikigasa <km3t1114@yahoo.co.jp>
2024-06-06 15:38:06 +0900

    fix: typo

--- backend/internal/adapter/middleware/cors.go
+++ backend/internal/adapter/middleware/cors.go
@@ -12,7 +12,7 @@ func SetupCORS() echo.MiddlewareFunc {
 	// Productionのみ限定的なスコープでのCORS
 	case config.EnvProduction:
 		InitWhiteList()
-		return AllowrRstrictiveOrigins()
+		return AllowRestrictiveOrigins()
 	case config.EnvDevelopment:
 		return AllowAllOrigins()
 	default:
@@ -54,7 +54,7 @@ func InitWhiteList() {
 	}
 }
 
-func AllowrRstrictiveOrigins() echo.MiddlewareFunc {
+func AllowRestrictiveOrigins() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			requestAddr := c.Request().Header.Get("Origin")
