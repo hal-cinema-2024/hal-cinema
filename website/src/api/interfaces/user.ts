ca0c402852f7eec209ccddd9c21860e4e68c4345
taku10101 <taku1011ht@gmail.com>
2024-06-07 10:36:34 +0900

    add userinterface

---
+++ website/src/api/interfaces/user.ts
@@ -0,0 +1,11 @@
+import { V1GetUserResponse } from "../../../../api/@types/index.ts";
+import { V1User } from "../../../../api/@types/index.ts";
+import { V1DeleteUserResponse } from "../../../../api/@types/index.ts";
+import { V1UpdateUserResponse } from "../../../../api/@types/index.ts";
+
+export type {
+  V1GetUserResponse as GetUserResponseInterface,
+  V1User as UserInterface,
+  V1DeleteUserResponse as DeleteUserResponseInterface,
+  V1UpdateUserResponse as UpdateUserResponseInterface,
+};
