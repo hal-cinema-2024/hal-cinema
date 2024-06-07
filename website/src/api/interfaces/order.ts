bf894e329598d0d3b23aa507891d1c3501fed4d5
taku10101 <taku1011ht@gmail.com>
2024-06-07 11:03:36 +0900

    add order Interface

--- website/src/api/interfaces/order.ts
+++ website/src/api/interfaces/order.ts
@@ -1,1 +1,17 @@
-import {} from "../../../../api/@types/index.ts";
+import {
+  V1Orders,
+  V1GetOrdersResponse,
+  V1CreateOrderRequest,
+  V1CreateOrderResponse,
+  V1GetOrderResponse,
+  V1DeleteOrderResponse,
+} from "../../../../api/@types/index.ts";
+
+export type {
+  V1Orders as OrdersInterface,
+  V1GetOrdersResponse as GetOrdersResponseInterface,
+  V1CreateOrderRequest as CreateOrderRequestBodyInterface,
+  V1CreateOrderResponse as CreateOrderResponseInterface,
+  V1GetOrderResponse as GetOrderResponseInterface,
+  V1DeleteOrderResponse as DeleteOrderResponseInterface,
+};
