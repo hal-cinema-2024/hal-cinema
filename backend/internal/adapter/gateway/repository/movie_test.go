a0a3b1060a0c7d0de2b6f4be147def74764979a4
sikigasa <km3t1114@yahoo.co.jp>
2024-06-14 10:35:53 +0900

    [WIP] update_movie_test

--- backend/internal/adapter/gateway/repository/movie_test.go
+++ backend/internal/adapter/gateway/repository/movie_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hal-cinema-2024/backend/internal/adapter/gateway/repository"
 	"github.com/hal-cinema-2024/backend/internal/entities/model"
 	"github.com/hal-cinema-2024/backend/internal/test"
+	"github.com/jackc/pgerrcode"
 	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
@@ -26,7 +27,7 @@ func TestCreateMovie(t *testing.T) {
 	}
 	movieRepo := repository.NewMovieRepo(db)
 
-	movie := &model.Movie{
+	movie := model.Movie{
 		MovieID:       uuid.NewString(),
 		Name:          "test",
 		Director:      "testDirector",
@@ -38,75 +39,6 @@ func TestCreateMovie(t *testing.T) {
 		EndDate:       time.Now(),
 	}
 
-	imagePaths := []string{
-		"test1",
-		"test2",
-		"test3",
-	}
-
-	createdMovieID, err := movieRepo.CreateMovie(ctx, movie, imagePaths)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Assert movie is created successfully
-	if createdMovieID == "" {
-		t.Error("Expected movie ID to be non-empty, but got empty")
-	}
-
-	// Assert movie is retrieved correctly
-	createdMovie, retrievedImagePaths, err := movieRepo.GetMovieByID(ctx, createdMovieID)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if createdMovie.Name != movie.Name {
-		t.Errorf("Expected movie name %s, but got %s", movie.Name, createdMovie.Name)
-	}
-
-	if createdMovie.Director != movie.Director {
-		t.Errorf("Expected movie director %s, but got %s", movie.Director, createdMovie.Director)
-	}
-
-	if createdMovie.Summary != movie.Summary {
-		t.Errorf("Expected movie summary %s, but got %s", movie.Summary, createdMovie.Summary)
-	}
-
-	if createdMovie.Link != movie.Link {
-		t.Errorf("Expected movie link %s, but got %s", movie.Link, createdMovie.Link)
-	}
-
-	if createdMovie.Term != movie.Term {
-		t.Errorf("Expected movie term %d, but got %d", movie.Term, createdMovie.Term)
-	}
-
-	if !reflect.DeepEqual(retrievedImagePaths, imagePaths) {
-		t.Errorf("Expected image paths %v, but got %v", imagePaths, retrievedImagePaths)
-	}
-}
-func TestGetMovieByID(t *testing.T) {
-	ctx := context.Background()
-	if err := test.NewContainer(t); err != nil {
-		t.Fatal(err)
-	}
-	db, err := invoke[*gorm.DB]()
-	if err != nil {
-		t.Fatal(err)
-	}
-	movieRepo := repository.NewMovieRepo(db)
-
-	// Create a test movie
-	movie := &model.Movie{
-		MovieID:       uuid.NewString(),
-		Name:          "test",
-		Director:      "testDirector",
-		Summary:       "testSummary",
-		ThumbnailPath: "testThumbnailPath",
-		Link:          "testLink",
-		Term:          120,
-		ReleaseDate:   time.Now(),
-		EndDate:       time.Now(),
-	}
 	imagePaths := []string{
 		"test1",
 		"test2",
@@ -116,27 +48,69 @@ func TestGetMovieByID(t *testing.T) {
 	testCases := []struct {
 		name        string
 		movie       model.Movie
+		imagePaths  []string
 		wantErrCode string
 	}{
 		{
 			name:        "success - get movie by ID",
-			movie:       *movie,
+			movie:       movie,
+			imagePaths:  imagePaths,
 			wantErrCode: "",
 		},
 		{
-			name:        "fail - record not found",
-			movie:       model.Movie{},
-			wantErrCode: "record not found",
+			name:        "success - get movie by ID no images",
+			movie:       movie,
+			imagePaths:  []string{},
+			wantErrCode: "",
 		},
 		{
-			name:        "fail - invalid UUID",
-			movie:       model.Movie{MovieID: "invalid-uuid"},
-			wantErrCode: "22P02",
+			name: "fail - null movie_id",
+			movie: model.Movie{
+				Name:          "test",
+				Director:      "testDirector",
+				Summary:       "testSummary",
+				ThumbnailPath: "testThumbnailPath",
+				Link:          "testLink",
+				Term:          120,
+				ReleaseDate:   time.Now(),
+				EndDate:       time.Now(),
+			},
+			imagePaths:  []string{},
+			wantErrCode: pgerrcode.NotNullViolation,
+		},
+		{
+			name: "fail - null required field",
+			movie: model.Movie{
+				MovieID:     uuid.NewString(),
+				Director:    "testDirector",
+				Term:        120,
+				ReleaseDate: time.Now(),
+				EndDate:     time.Now(),
+			},
+			imagePaths:  []string{},
+			wantErrCode: pgerrcode.NotNullViolation,
+		},
+		{
+			name: "fail - invalid term",
+			movie: model.Movie{
+				MovieID:       uuid.NewString(),
+				Name:          "test",
+				Director:      "testDirector",
+				Summary:       "testSummary",
+				ThumbnailPath: "testThumbnailPath",
+				Link:          "testLink",
+				Term:          -1,
+				ReleaseDate:   time.Now(),
+				EndDate:       time.Now(),
+			},
+			imagePaths:  []string{},
+			wantErrCode: pgerrcode.CheckViolation,
 		},
 	}
+
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			_, err := movieRepo.CreateMovie(ctx, &tc.movie, imagePaths)
+			_, err := movieRepo.CreateMovie(ctx, &tc.movie, tc.imagePaths)
 			if err != nil {
 				var pgErr *pq.Error
 				if errors.As(err, &pgErr) {
@@ -150,36 +124,114 @@ func TestGetMovieByID(t *testing.T) {
 		})
 	}
 
-	// Retrieve the created movie
-	createdMovie, retrievedImagePaths, err := movieRepo.GetMovieByID(ctx, movie.MovieID)
+}
+func TestGetMovieByID(t *testing.T) {
+	ctx := context.Background()
+	if err := test.NewContainer(t); err != nil {
+		t.Fatal(err)
+	}
+	db, err := invoke[*gorm.DB]()
+	if err != nil {
+		t.Fatal(err)
+	}
+	movieRepo := repository.NewMovieRepo(db)
+
+	// Create a test movie
+	movie := model.Movie{
+		MovieID:       uuid.NewString(),
+		Name:          "test",
+		Director:      "testDirector",
+		Summary:       "testSummary",
+		ThumbnailPath: "testThumbnailPath",
+		Link:          "testLink",
+		Term:          120,
+		ReleaseDate:   time.Now(),
+		EndDate:       time.Now(),
+	}
+	imagePaths := []string{
+		"test1",
+		"test2",
+		"test3",
+	}
+	_, err = movieRepo.CreateMovie(ctx, &movie, imagePaths)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Assert movie is retrieved correctly
-	if createdMovie.Name != movie.Name {
-		t.Errorf("Expected movie name %s, but got %s", movie.Name, createdMovie.Name)
+	testCases := []struct {
+		name        string
+		movie       model.Movie
+		imagePaths  []string
+		wantErrCode string
+	}{
+		{
+			name:        "success - get movie by ID",
+			movie:       movie,
+			imagePaths:  imagePaths,
+			wantErrCode: "",
+		},
+		{
+			name:        "success - get movie by ID no images",
+			movie:       movie,
+			imagePaths:  []string{},
+			wantErrCode: "",
+		},
+		{
+			name:        "fail - record not found",
+			movie:       model.Movie{},
+			imagePaths:  []string{},
+			wantErrCode: "record not found",
+		},
+		{
+			name:        "fail - invalid UUID",
+			movie:       model.Movie{MovieID: "invalid-uuid2"},
+			imagePaths:  []string{},
+			wantErrCode: "22P02",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			// Retrieve the created movie
+			createdMovie, retrievedImagePaths, err := movieRepo.GetMovieByID(ctx, movie.MovieID)
+			if err != nil {
+				var pgErr *pq.Error
+				if errors.As(err, &pgErr) {
+					if string(pgErr.Code) == tc.wantErrCode {
+						t.Log(err)
+						return
+					}
+					t.Errorf("got %v; want %v", err, tc.wantErrCode)
+				}
+			}
+
+			// Assert movie is retrieved correctly
+			if createdMovie.Name != movie.Name {
+				t.Errorf("Expected movie name %s, but got %s", movie.Name, createdMovie.Name)
+			}
+
+			if createdMovie.Director != movie.Director {
+				t.Errorf("Expected movie director %s, but got %s", movie.Director, createdMovie.Director)
+			}
+
+			if createdMovie.Summary != movie.Summary {
+				t.Errorf("Expected movie summary %s, but got %s", movie.Summary, createdMovie.Summary)
+			}
+
+			if createdMovie.Link != movie.Link {
+				t.Errorf("Expected movie link %s, but got %s", movie.Link, createdMovie.Link)
+			}
+
+			if createdMovie.Term != movie.Term {
+				t.Errorf("Expected movie term %d, but got %d", movie.Term, createdMovie.Term)
+			}
+
+			if !reflect.DeepEqual(retrievedImagePaths, imagePaths) {
+				t.Errorf("Expected image paths %v, but got %v", imagePaths, retrievedImagePaths)
+			}
+		})
 	}
 
-	if createdMovie.Director != movie.Director {
-		t.Errorf("Expected movie director %s, but got %s", movie.Director, createdMovie.Director)
-	}
-
-	if createdMovie.Summary != movie.Summary {
-		t.Errorf("Expected movie summary %s, but got %s", movie.Summary, createdMovie.Summary)
-	}
-
-	if createdMovie.Link != movie.Link {
-		t.Errorf("Expected movie link %s, but got %s", movie.Link, createdMovie.Link)
-	}
-
-	if createdMovie.Term != movie.Term {
-		t.Errorf("Expected movie term %d, but got %d", movie.Term, createdMovie.Term)
-	}
-
-	if !reflect.DeepEqual(retrievedImagePaths, imagePaths) {
-		t.Errorf("Expected image paths %v, but got %v", imagePaths, retrievedImagePaths)
-	}
 }
 
 func TestGetMovies(t *testing.T) {
