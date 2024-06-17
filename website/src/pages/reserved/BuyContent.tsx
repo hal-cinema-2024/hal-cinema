361a77e4db1d03594570b2aa86fc6ce9d5b851eb
hikaru1309 <hikarishiro2001@icloud.com>
2024-06-17 17:40:44 +0900

    fix: reserved-table

--- website/src/pages/reserved/BuyContent.tsx
+++ website/src/pages/reserved/BuyContent.tsx
@@ -1,78 +1,35 @@
-import styled from "styled-components";
+import "./Table.css";
 
 const BuyContent = () => {
   return (
     <>
-      <TitleContainer>
+      <div className="TitleContainer">
         <p>購入内容</p>
-      </TitleContainer>
-      <TableCustom>
-        <TrCustom>
-          <ThCustom>映画名</ThCustom>
-          <TdCustom>海野翔太</TdCustom>
-        </TrCustom>
-        <TrCustom>
-          <ThCustom>スクリーン</ThCustom>
-          <TdCustom>ウンノショウタ</TdCustom>
-        </TrCustom>
-        <TrCustom>
-          <ThCustom>座席</ThCustom>
-          <TdCustom>nagg@hal.ac.jp</TdCustom>
-        </TrCustom>
-        <TrCustom>
-          <ThCustom>上映時間</ThCustom>
-          <TdCustom>nagg@hal.ac.jp</TdCustom>
-        </TrCustom>
-        <TrCustom>
-          <ThCustom>金額</ThCustom>
-          <TdCustom>nagg@hal.ac.jp</TdCustom>
-        </TrCustom>
-      </TableCustom>
+      </div>
+      <table>
+        <tr>
+          <th>映画名</th>
+          <td>海野翔太</td>
+        </tr>
+        <tr>
+          <th>スクリーン</th>
+          <td>ウンノショウタ</td>
+        </tr>
+        <tr>
+          <th>座席</th>
+          <td>nagg@hal.ac.jp</td>
+        </tr>
+        <tr>
+          <th>上映日時</th>
+          <td>nagg@hal.ac.jp</td>
+        </tr>
+        <tr>
+          <th>金額</th>
+          <td>nagg@hal.ac.jp</td>
+        </tr>
+      </table>
     </>
   );
 };
 
 export default BuyContent;
-
-// ----style----
-const TitleContainer = styled.div`
-  margin: 0 0 15px 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  p {
-    font-size: 20px;
-  }
-  button {
-    padding: 3px 30px;
-    font-size: 14px;
-    background-color: #fff;
-    color: #000;
-    border-radius: 5px;
-  }
-`;
-
-const TableCustom = styled.table`
-  width: 100%;
-`;
-
-const TrCustom = styled.tr`
-  color: #000000;
-  border-bottom: 1px solid #333;
-`;
-
-const ThCustom = styled.th`
-  width: 30%;
-  padding: 20px 0;
-  text-align: center;
-  border-right: 1px solid #333;
-  background-color: #b7b7b7;
-`;
-
-const TdCustom = styled.td`
-  width: 70%;
-  padding: 20px 0;
-  text-align: center;
-  background-color: #ffffff;
-`;
--- website/src/pages/reserved/CustomerInfo.tsx
+++ website/src/pages/reserved/CustomerInfo.tsx
@@ -1,71 +1,28 @@
-import styled from "styled-components";
+import "./Table.css";
 
 const CustomerInfo = () => {
   return (
     <>
-      <TitleContainer>
+      <div className="TitleContainer">
         <p>お客様情報</p>
         <button>修正する</button>
-      </TitleContainer>
-      <TableCustom>
-        <TrCustom>
-          <ThCustom>お名前</ThCustom>
-          <TdCustom>海野翔太</TdCustom>
-        </TrCustom>
-        <TrCustom>
-          <ThCustom>フリガナ</ThCustom>
-          <TdCustom>ウンノショウタ</TdCustom>
-        </TrCustom>
-        <TrCustom>
-          <ThCustom>メールアドレス</ThCustom>
-          <TdCustom>nagg@hal.ac.jp</TdCustom>
-        </TrCustom>
-      </TableCustom>
+      </div>
+      <table>
+        <tr>
+          <th>お名前</th>
+          <td>海野翔太</td>
+        </tr>
+        <tr>
+          <th>フリガナ</th>
+          <td>ウンノショウタ</td>
+        </tr>
+        <tr>
+          <th>メールアドレス</th>
+          <td>nagg@hal.ac.jp</td>
+        </tr>
+      </table>
     </>
   );
 };
 
 export default CustomerInfo;
-
-// ----style----
-const TitleContainer = styled.div`
-  margin: 0 0 15px 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  p {
-    font-size: 20px;
-  }
-  button {
-    padding: 3px 30px;
-    font-size: 14px;
-    background-color: #fff;
-    color: #000;
-    border-radius: 5px;
-  }
-`;
-
-const TableCustom = styled.table`
-  width: 100%;
-`;
-
-const TrCustom = styled.tr`
-  color: #000000;
-  border-bottom: 1px solid #333;
-`;
-
-const ThCustom = styled.th`
-  width: 30%;
-  padding: 20px 0;
-  text-align: center;
-  border-right: 1px solid #333;
-  background-color: #b7b7b7;
-`;
-
-const TdCustom = styled.td`
-  width: 70%;
-  padding: 20px 0;
-  text-align: center;
-  background-color: #ffffff;
-`;
