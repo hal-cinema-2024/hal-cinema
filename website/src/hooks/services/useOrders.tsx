e777789b49c15ac84d03673e0763469ab4e9b7b1
taku10101 <taku1011ht@gmail.com>
2024-06-22 07:32:09 +0900

    add orders hooks

--- website/src/hooks/services/useOrders.tsx
+++ website/src/hooks/services/useOrders.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { GetOrdersResponseInterface } from "../../api/interfaces/order";
+import { getOrders } from "../../api/repositories/order";
 
-export const useOrders = () => {
-  return <div>useOrders</div>;
+export const useOrders = (userId?: string) => {
+  const [orders, setOrders] = useState<GetOrdersResponseInterface>();
+
+  const fetchData = async (userId?: string) => {
+    try {
+      const res = await getOrders(userId);
+      if (res) setOrders(res);
+    } catch (error) {
+      console.error("orders service error: " + error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(userId);
+  }, []);
+
+  return { orders, setOrders };
 };
