b7aa25d503aeded272f89ed43bd48c7742ea1dae
taku10101 <taku1011ht@gmail.com>
2024-06-22 07:37:59 +0900

    add Schedules hooks

--- website/src/hooks/services/useSchedules.tsx
+++ website/src/hooks/services/useSchedules.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { GetSchedulesResponseInterface } from "../../api/interfaces/schedule";
+import { getSchedules } from "../../api/repositories/schedule";
 
-export const useSchedules = () => {
-  return <div>useSchedules</div>;
+export const useSchedules = (
+  pageId?: string,
+  pageSize?: string,
+  date?: string
+) => {
+  const [schedules, setSchedules] = useState<GetSchedulesResponseInterface>();
+  const fetchData = async (
+    pageId?: string,
+    pageSize?: string,
+    date?: string
+  ) => {
+    try {
+      const res = await getSchedules(
+        pageId,
+        pageSize,
+        date ? new Date(date).toISOString() : undefined
+      );
+      if (res) setSchedules(res);
+    } catch (error) {
+      console.error("schedules service error: " + error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(
+      pageId,
+      pageSize,
+      date ? new Date(date).toISOString() : undefined
+    );
+  }, []);
+
+  return { schedules, setSchedules };
 };
