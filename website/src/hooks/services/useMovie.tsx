da50d4a1711551fe0de287651ac1822e282bf9f3
taku10101 <taku1011ht@gmail.com>
2024-06-22 07:19:48 +0900

    add movie hooks

--- website/src/hooks/services/useMovie.tsx
+++ website/src/hooks/services/useMovie.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { GetMovieResponseInterface } from "../../api/interfaces/movie";
+import { getMovie } from "../../api/repositories/movie";
+export const useMovie = (movieId: string) => {
+  const [movie, setMovie] = useState<GetMovieResponseInterface>({});
 
-export const useMovie = () => {
-  return <div>useMovie</div>;
+  const fetchData = async (movieId: string) => {
+    try {
+      const res = await getMovie(movieId);
+      if (res) setMovie(res);
+    } catch (error) {
+      console.error("movie service error: " + error);
+    }
+  };
+  useEffect(() => {
+    fetchData(movieId);
+  }, []);
+
+  return { movie, setMovie };
 };
