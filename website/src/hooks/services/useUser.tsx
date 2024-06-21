0c49fcd6302fd76e7f47ba947574a23b7a10a1d2
taku10101 <taku1011ht@gmail.com>
2024-06-22 07:43:50 +0900

    add user hooks

--- website/src/hooks/services/useUser.tsx
+++ website/src/hooks/services/useUser.tsx
@@ -1,3 +1,21 @@
-export const useUser = () => {
-  return <div>useUser</div>;
+import { useEffect, useState } from "react";
+import { getUser } from "../../api/repositories/user";
+import { GetUserResponseInterface } from "../../api/interfaces/user";
+
+export const useUser = (userId: string) => {
+  const [user, setUser] = useState<GetUserResponseInterface>();
+
+  const fetchData = async (userId: string) => {
+    try {
+      const res = await getUser(userId);
+      if (res) setUser(res);
+    } catch (error) {
+      console.error("user service error: " + error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(userId);
+  }, []);
+  return { user, setUser };
 };
