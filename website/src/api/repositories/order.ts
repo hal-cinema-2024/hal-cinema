ecf9c77ec29a827c4cde4f94d97ce9ab289879fc
taku10101 <taku1011ht@gmail.com>
2024-06-21 10:48:54 +0900

    add order repo

--- website/src/api/repositories/order.ts
+++ website/src/api/repositories/order.ts
@@ -1,5 +1,6 @@
 import { client } from "../../utils/aspida";
 import {
+  CreateOrderRequestBodyInterface,
   GetOrderResponseInterface,
   GetOrdersResponseInterface,
 } from "../interfaces/order";
@@ -21,6 +22,26 @@ export const getOrder = async (orderId: string) => {
       ._orderId(orderId)
       .$get();
     return res;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const createOrder = async (
+  requestBody: CreateOrderRequestBodyInterface
+) => {
+  try {
+    await client.v1.orders.$post({
+      body: requestBody,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteOrder = async (orderId: string) => {
+  try {
+    await client.v1.orders._orderId(orderId).$delete();
   } catch (err) {
     console.log(err);
   }
