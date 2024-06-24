924d4dede6c18b75fb2b11578e54e1da6714ed36
taku10101 <taku1011ht@gmail.com>
2024-06-24 15:03:54 +0900

    add admin repo

---
+++ admin/src/api/interfaces/user.ts
@@ -0,0 +1,23 @@
+import {
+  V1User,
+  V1GetUserResponse,
+  V1UpdateUserResponse,
+  V1DeleteUserResponse,
+  V1GetUsersResponse,
+} from "../../../../api/@types/index";
+
+type UpdateUserRequestInterface = {
+  firstName?: string | undefined;
+  lastName?: string | undefined;
+  firstNameReading?: string | undefined;
+  lastNameReading?: string | undefined;
+  gender?: number | undefined;
+};
+export type {
+  V1User as UserInterface,
+  V1GetUsersResponse as GetUsersResponseInterface,
+  V1GetUserResponse as GetUserResponseInterface,
+  V1UpdateUserResponse as UpdateUserResponseInterface,
+  UpdateUserRequestInterface,
+  V1DeleteUserResponse as DeleteUserResponseInterface,
+};
