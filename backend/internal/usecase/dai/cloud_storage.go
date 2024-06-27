225a1d9439d89248f30131d7014495bf505aa5b6
sikigasa <km3t1114@yahoo.co.jp>
2024-06-27 10:55:41 +0900

    add: delete_blob

--- backend/internal/usecase/dai/cloud_storage.go
+++ backend/internal/usecase/dai/cloud_storage.go
@@ -4,4 +4,5 @@ import "context"
 
 type CloudStorage interface {
 	UploadBlob(ctx context.Context, fileName string, fileData []byte) (string, error)
+	DeleteBlob(ctx context.Context, blobName string) error
 }
