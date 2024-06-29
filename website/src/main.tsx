5055f2ef144bac8a33360b5dcc99bc81e88571c7
taku10101 <taku1011ht@gmail.com>
2024-06-30 00:48:07 +0900

    fetch user

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UIProvider } from "@yamada-ui/react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { UserIdProvider } from "./routes/google/callback/-store/useUserIdContext.tsx";
+import { UserIdProvider } from "./-store/useUserIdContext.tsx";
 
 const queryClient = new QueryClient();
 const CLIENT_ID: string = import.meta.env.VITE_GOOGLE_CLIENT_ID;
