71668a99deaeb3cd5173245258aefc5e80636ce4
taku10101 <taku1011ht@gmail.com>
2024-07-01 15:28:56 +0900

    fix schedule type

--- website/src/routes/schedules/utils/TransSchedule.ts
+++ website/src/routes/schedules/utils/TransSchedule.ts
@@ -1,38 +1,35 @@
 import { ScheduleInterface } from "../../../../../fe-api/interfaces/schedule";
-import { TransformedData } from "../types/TransFormData";
+import { TheaterSchedule, TransformedData } from "../types/TransFormData";
 
 export function transformData(data: ScheduleInterface[]): TransformedData[] {
-  const result: { [movieName: string]: TransformedData } = {};
+  const groupedByMovie: {
+    [movieName: string]: { [theater: string]: TheaterSchedule };
+  } = {};
 
   data.forEach((item) => {
-    const { movieName, theaterId, scheduleId, startTime, term, isFull } = item;
+    if (item.movieName && item.theater) {
+      if (!groupedByMovie[item.movieName]) {
+        groupedByMovie[item.movieName] = {};
+      }
 
-    // Skip invalid data
-    if (!movieName || !theaterId) {
-      return;
+      if (!groupedByMovie[item.movieName][item.theater]) {
+        groupedByMovie[item.movieName][item.theater] = {
+          theater: item.theater,
+          schedules: [],
+        };
+      }
+
+      groupedByMovie[item.movieName][item.theater].schedules.push({
+        scheduleId: item.scheduleId || "",
+        startTime: item.startTime || "",
+        endTime: item.endTime || "",
+        isAvailable: item.isAvailable || false,
+      });
     }
-
-    // Check if movieName exists in result, if not, create a new entry
-    if (!result[movieName]) {
-      result[movieName] = {
-        movieName: movieName,
-        theaterSchedule: {},
-      };
-    }
-
-    // Check if theater exists in theaterSchedule, if not, create a new entry
-    if (!result[movieName].theaterSchedule[theaterId]) {
-      result[movieName].theaterSchedule[theaterId] = [];
-    }
-
-    // Add the schedule to the appropriate theater
-    result[movieName].theaterSchedule[theaterId].push({
-      scheduleId,
-      startTime,
-      term,
-      isFull,
-    });
   });
 
-  return Object.values(result);
+  return Object.keys(groupedByMovie).map((movieName) => ({
+    movieName,
+    theaterSchedule: Object.values(groupedByMovie[movieName]),
+  }));
 }
