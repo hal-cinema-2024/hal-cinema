ac9c855b8b7a43f7a5c30a5779ade96d32e678be
taku10101 <taku1011ht@gmail.com>
2024-07-05 03:35:09 +0900

    schema 適応

--- website/src/routes/schedules/$scheduleId/form/-components/select_ticket/TicketForm.tsx
+++ website/src/routes/schedules/$scheduleId/form/-components/select_ticket/TicketForm.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@yamada-ui/react";
 import { useForm, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
+import { ticketFormSchema } from "./TicketSchema";
 export function MovieFormProvider() {
-  const methods = useForm({ resolver: zodResolver(movieFormSchema) });
+  const methods = useForm({ resolver: zodResolver(ticketFormSchema) });
   const { handleSubmit } = methods;
   return (
     <FormProvider {...methods}>
