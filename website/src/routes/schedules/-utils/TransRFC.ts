182286dd13d1591076e1ddd5e3bfc14ae4d04283
taku10101 <taku1011ht@gmail.com>
2024-07-05 02:09:59 +0900

    fix

--- website/src/routes/schedules/-utils/TransRFC.ts
+++ website/src/routes/schedules/-utils/TransRFC.ts
@@ -1,9 +1,15 @@
-export function getRFCDate(month: number, day: number, week: number): string {
+import { SlideDateType } from "../-types/SlideDate";
+
+export function TransRFCDate(data: SlideDateType): string {
   const now = new Date();
   const currentYear = now.getFullYear();
 
   // 引数で受け取った月、日、週を適用した日付を作成
-  const date = new Date(currentYear, month - 1, day + week * 7);
+  const date = new Date(
+    currentYear,
+    parseInt(data.month) - 1,
+    parseInt(data.day)
+  );
 
   // RFC形式の日付文字列を返す
   return date.toISOString();
