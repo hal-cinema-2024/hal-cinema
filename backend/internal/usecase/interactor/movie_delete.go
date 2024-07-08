4a8df5d4b91c972a3e6d7fe974ae9f9642d4e96e
sikigasa <km3t1114@yahoo.co.jp>
2024-07-08 15:46:49 +0900

    update: update_movie interactor

--- backend/internal/usecase/interactor/movie_delete.go
+++ backend/internal/usecase/interactor/movie_delete.go
@@ -1,9 +1,44 @@
 package interactor
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 func (mi *MovieInteractor) DeleteMovie(ctx context.Context, movieID string) error {
-	err := mi.Repositories.DeleteMovie(ctx, movieID)
+	var wg sync.WaitGroup
+
+	_, imagePaths, err := mi.Repositories.GetMovieByID(ctx, movieID)
+	if err != nil {
+		return err
+	}
+	errChan := make(chan error, len(imagePaths))
+
+	// 指定の画像を削除
+	for _, imagePath := range imagePaths {
+		wg.Add(1)
+		go func(imagePath string) {
+			defer wg.Done()
+			err := mi.cloudStorage.DeleteBlob(ctx, imagePath)
+			if err != nil {
+				errChan <- err
+				return
+			}
+		}(imagePath)
+	}
+
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	for err := range errChan {
+		return err
+	}
+
+	wg.Wait()
+
+	err = mi.Repositories.DeleteMovie(ctx, movieID)
 	if err != nil {
 		return err
 	}
