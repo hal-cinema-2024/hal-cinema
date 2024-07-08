80a16d1f3477462f6da9dda1cc628b954eff8683
sikigasa <km3t1114@yahoo.co.jp>
2024-07-08 15:31:51 +0900

    update: azure.go

--- backend/internal/adapter/gateway/cloudstorage/azure/azure.go
+++ backend/internal/adapter/gateway/cloudstorage/azure/azure.go
@@ -3,8 +3,10 @@ package azure
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/google/uuid"
 	"github.com/hal-cinema-2024/backend/cmd/config"
 )
 
@@ -17,7 +19,12 @@ func NewAzureCloudStorage(bclient *azblob.Client) *AzureCloudStorage {
 }
 
 func (cs *AzureCloudStorage) UploadBlob(ctx context.Context, fileName string, fileData []byte) (string, error) {
-	_, err := cs.bclient.UploadBuffer(ctx, config.Config.Azure.BlobServiceContainerName, fileName, fileData, &azblob.UploadBufferOptions{})
+	uuid, err := uuid.NewV7()
+	if err != nil {
+		return "", err
+	}
+	fileName = fmt.Sprintf("%s-%s", uuid, fileName)
+	_, err = cs.bclient.UploadBuffer(ctx, config.Config.Azure.BlobServiceContainerName, fileName, fileData, &azblob.UploadBufferOptions{})
 	if err != nil {
 		return "", err
 	}
@@ -27,7 +34,14 @@ func (cs *AzureCloudStorage) UploadBlob(ctx context.Context, fileName string, fi
 // delete blob from blob storage
 func (cs *AzureCloudStorage) DeleteBlob(ctx context.Context, blobName string) error {
 	var containerName string = config.Config.Azure.BlobServiceContainerName
-	_, err := cs.bclient.DeleteBlob(ctx, containerName, blobName, &azblob.DeleteBlobOptions{})
+	var fileName string
+	if strings.Contains(blobName, config.Config.Azure.BlobServiceURL) {
+		target := config.Config.Azure.BlobServiceURL + containerName + "/"
+		fileName = strings.Replace(blobName, target, "", -1)
+	} else {
+		fileName = blobName
+	}
+	_, err := cs.bclient.DeleteBlob(ctx, containerName, fileName, &azblob.DeleteBlobOptions{})
 	if err != nil {
 		return err
 	}
