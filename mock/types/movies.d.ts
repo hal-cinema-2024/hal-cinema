a9347fe5f31f450e91ffa23ff6a492454631cb36
taku10101 <taku1011ht@gmail.com>
2024-11-05 01:17:54 +0900

    json server set

---
+++ mock/types/movies.d.ts
@@ -0,0 +1,12 @@
+interface Movies {
+  movieId?: string;
+  movieName?: string;
+  director?: string;
+  thumbnail?: string;
+  summary?: string;
+  link?: string;
+  term?: number;
+  releaseDate?: string;
+  endDate?: string;
+  movieImage?: string[];
+}
