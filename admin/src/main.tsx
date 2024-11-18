833be000792cee265b16a6da0e62b5f4d2162169
syoufiiii <tanabesyoukaic25.k13@icloud.com>
2024-11-18 12:24:05 +0900

    add:admin再形成

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,22 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { UIProvider } from "@yamada-ui/react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { UserIdProvider } from "./store/useUserIdContext.tsx";
-
-const queryClient = new QueryClient();
-const CLIENT_ID: string = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <UIProvider>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
-      <QueryClientProvider client={queryClient}>
-        <UserIdProvider>
-          <App />
-        </UserIdProvider>
-      </QueryClientProvider>
-    </GoogleOAuthProvider>
-  </UIProvider>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- admin/src/routes/__root.tsx
+++
@@ -1,7 +0,0 @@
-import { createRootRoute } from "@tanstack/react-router";
-import CommonLayout from "../components/commonlayout";
-//import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-
-export const Route = createRootRoute({
-  component: CommonLayout,
-});
