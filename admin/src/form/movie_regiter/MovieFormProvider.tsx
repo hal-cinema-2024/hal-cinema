90204c49fa3e34940f4fcd6dd8c05099ae888aa4
taku10101 <taku1011ht@gmail.com>
2024-11-21 15:53:47 +0900

    formライブラリ追加

--- admin/src/form/movie_regiter/MovieFormProvider.tsx
+++ admin/src/form/movie_regiter/MovieFormProvider.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@nextui-org/react";
+import { Button } from "@yamada-ui/react";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { InputField } from "../InputField";
@@ -13,7 +13,6 @@ export function MovieFormProvider() {
   return (
     <FormProvider {...methods}>
       <form onSubmit={handleSubmit((data) => console.log(data))}>
-        {/* 各入力フィールドに異なる fieldName を渡します */}
         <InputField fieldName='movieName' />
         <InputField fieldName='director' />
         <InputField fieldName='summary' />
--- admin/src/form/profile_form/genderOption.ts
+++
@@ -1,5 +0,0 @@
-export const option = [
-  { value: "0", label: "男性" },
-  { value: "1", label: "女性" },
-  { value: "2", label: "回答しない" },
-];
