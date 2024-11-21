6a76e7b3ee985e54a4949beda94a298f41b2b4b0
taku10101 <taku1011ht@gmail.com>
2024-11-21 13:21:05 +0900

    hook追加

---
+++ mock/hooks/useMovies.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { MoviesMock } from "../types/movies";
+export const useMovies = (pageId: string, pageSize: string) => {
+  const [movies, setMovies] = useState<MoviesMock[]>([]);
+
+  const fetchData = async (pageId?: string, pageSize?: string) => {
+    try {
+      const url = "http://localhost:8080/movies";
+      const res = await fetch(url + `?pageId=${pageId}&pageSize=${pageSize}`);
+      const data = await res.json();
+      setMovies(data);
+    } catch (error) {
+      console.error("movie service error: " + error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(pageId, pageSize);
+  }, [pageId, pageSize]);
+
+  return { movies, setMovies };
+};
