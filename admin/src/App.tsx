c8fcb2a30e89e03705e72701d4f0c3fa040035a1
hikaru1309 <hikarishiro2001@icloud.com>
2024-11-25 12:27:58 +0900

    fix: adminのRoute

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { MovieTable } from "./data-table/MovieTable";
 import { Home } from "./routes/Home";
+import { Layout } from "./components/Layout";
 
 const App: React.FC = () => {
   return (
-    <Router>
+
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<div></div>} />
+        <Route path="/" element={<Home />}/>
+          <Route  element={<Layout />} >
           <Route path="movies" element={<MovieTable />} />
           <Route path="movies/:movieId" element={<MovieTable />} />
         </Route>
       </Routes>
-    </Router>
   );
 };
 
--- admin/src/routes/Home.tsx
+++ admin/src/routes/Home.tsx
@@ -1,26 +1,3 @@
-import { Outlet } from 'react-router-dom';
-import "./style.css";
-
 export const Home = () => {
-  return (
-  <>
-    <div className="headerWrap">
-      <div>
-        <h1>HAL Cinema</h1>
-        <nav>
-          <ul>
-            <li>映画情報</li>
-            <li>上映スケジュール</li>
-            <li>利用者情報</li>
-            <li>予約情報</li>
-          </ul>
-        </nav>
-      </div>
-      <p>ログアウト</p>
-    </div>
-
-    <Outlet />
-  </>
-  );
+  return <>home</>;
 };
-
